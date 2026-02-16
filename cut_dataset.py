ed90e56267d43f6ddbaaedd300d0fd3f4e9537c1
RacFor4k <racfor4@gmail.com>
2026-02-16 11:57:14 +0300

    улучшен счетчик прогресса

--- cut_dataset.py
+++ cut_dataset.py
@@ -4,18 +4,20 @@ import tqdm
 
 SIZE_LIMIT = 100 * 1024 #512KB 
 
-def cut(path):
+def cut(path,tq):
     for file in os.listdir(path):
         with open(os.path.join(path,file), "r+b") as f:
             f.truncate(SIZE_LIMIT)
+            tq.update(1)
 
 def main():
     path = sys.argv[1]
     if not os.path.exists(path):
         raise "Invalid path"
-    types = os.listdir(path)
-    for type in tqdm.tqdm(types):
-        cut(os.path.join(path, type))
+    types = [i for i in os.listdir(path) if not os.path.path(i)]
+    tq = tqdm.tqdm(total=5000*len(types))
+    for type in types:
+        cut(os.path.join(path, type),tq)
 
 if __name__ == '__main__':
     main()
