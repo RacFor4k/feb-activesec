78247461b7f773e7185105750a102830a05f3e58
RacFor4k <racfor4@gmail.com>
2026-02-16 12:44:07 +0300

    изменен размер

--- AI/prepare.py
+++ AI/prepare.py
@@ -2,7 +2,7 @@ import os
 import sys
 import tqdm
 
-PACKET_SIZE = 10*1024 #10KB
+PACKET_SIZE = 100*1024 #100KB
 
 def create_config(types):
     with open(os.path.join('prepare', 'train.ignore')) as file:
@@ -10,9 +10,10 @@ def create_config(types):
             file.write(f'#{type}\n')
 
 def proccess(path, type):
-    for file in os.listdir(path):
+    str().lower
+    for i, file in enumerate([file for file in os.listdir(path) if file.split('.')[0].lower() != type]):
         with open(os.path.join(path,file), 'rb') as reader:
-            with open(os.path.join('prepared', type, file), 'wb') as writer:
+            with open(os.path.join('prepared', type, f'{i:04d}'), 'wb') as writer:
                 writer.write(reader.read(PACKET_SIZE))
 
 def main():
@@ -21,7 +22,7 @@ def main():
     path = sys.argv[1]
     if not os.path.exists(path):
         raise "Invalid path"
-    types = os.listdir(path)
+    types = [type[-6].lower() for type in os.listdir(path) if not os.path.isfile(type)]
     for type in tqdm.tqdm(types):
         proccess(os.path.join(path, type), type)
     create_config(types)
